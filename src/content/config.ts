5a957aa0af4817487eed3bf8dc8f4583ee871091
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2024-04-24 00:14:51 +0700

    Refactor code

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,28 @@
 import { defineCollection, z } from "astro:content";
 
-const showcase = defineCollection({
+const organizers = defineCollection({
   type: "data",
   schema: ({ image }) =>
     z.object({
-      title: z.string().min(1),
-      image: image(),
-      url: z.string().url(),
       featured: z.number().min(1).optional(),
+      image: image(),
+      name: z.string(),
+      width: z.string().optional(),
+    }),
+});
+
+const sponsors = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      featured: z.number().min(1).optional(),
+      image: image(),
+      name: z.string(),
+      width: z.string().optional(),
     }),
 });
 
 export const collections = {
-  showcase,
+  organizers,
+  sponsors,
 };

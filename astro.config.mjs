10a385229f0549955721c678cd6089c64cfc98d3
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2024-04-24 00:37:24 +0700

    Add logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import lottie from "astro-integration-lottie";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.hackathonkonyol.com/",
-  integrations: [tailwind(), icon()],
+  integrations: [
+    icon(),
+    lottie(),
+    tailwind(),
+  ],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro-integration-lottie/env" />
 /// <reference types="astro/client" />
